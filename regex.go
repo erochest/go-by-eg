21d5098dd623ee45dc7d0afe40e7e992c82a777b
Eric Rochester <err8n@virginia.edu>
2012-11-19 13:33:02 -0500

    Added code for regular expressions.

---
+++ regex.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"regexp"
+)
+
+func main() {
+	match, _ := regexp.MatchString("p([a-z]+)ch", "peach")
+	fmt.Println(match)
+
+	r, _ := regexp.Compile("p([a-z]+)ch")
+	p := "peach punch"
+
+	fmt.Println(r.MatchString("peach"))
+	fmt.Println(r.FindString(p))
+	fmt.Println(r.FindStringIndex(p))
+	fmt.Println(r.FindStringSubmatch(p))
+	fmt.Println(r.FindStringSubmatchIndex(p))
+	fmt.Println(r.FindAllString("peach punch pinch", -1))
+	fmt.Println(r.FindAllStringSubmatchIndex("peach punch pinch", -1))
+	fmt.Println(r.FindAllString("peach punch pinch", 2))
+	fmt.Println(r.Match([]byte("peach")))
+
+	r = regexp.MustCompile("p([a-z]+)ch")
+	fmt.Println(r)
+
+	fmt.Println(r.ReplaceAllString("a peach", "<fruit>"))
+
+	in := []byte("a peach")
+	out := r.ReplaceAllFunc(in, bytes.ToUpper)
+	fmt.Println(string(out))
+}
